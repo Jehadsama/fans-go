5ac221f0f80a171b7079356827cbd2bb546a8d3c
jehadsama <ayumijehad@qq.com>
2022-06-08 19:15:27 +0800

    feat(base.go): add base model

--- app/models/base.go
+++ app/models/base.go
@@ -1,15 +1,25 @@
 package models
 
-import "time"
+import (
+	"time"
 
-// gorm.Model 的定义
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"gorm.io/gorm"
+)
+
 type BaseModel struct {
-	// 其实是mongo objectid
-	ID string `gorm:"primaryKey;column:_id;"`
+	// 其实是mongo objectid, `gorm:"type:varchar(255);column:_id;primaryKey;not null;" json:"_id"`
+	ID string `gorm:"type:varchar(255);column:_id;primaryKey;not null;" json:"_id"`
 	// 创建时间
-	CreatedAt time.Time `gorm:"column:created_at"`
+	CreatedAt time.Time `json:"created_at"`
 	// 修改时间
-	UpdatedAt time.Time `gorm:"column:updated_at"`
+	UpdatedAt time.Time `json:"updated_at"`
 	// 是否删除
-	IsDel bool `gorm:"column:is_del;default:false"`
+	IsDel bool `gorm:"default:false" json:"is_del"`
+}
+
+func (b *BaseModel) BeforeCreate(tx *gorm.DB) (err error) {
+	id := primitive.NewObjectID().Hex()
+	b.ID = id
+	return
 }
