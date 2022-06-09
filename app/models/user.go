124ff464386ccdf39c7d33e9782188e0f896c02e
jehadsama <ayumijehad@qq.com>
2022-06-09 19:06:57 +0800

    feat(user,relationship): add models

--- app/models/user.go
+++ app/models/user.go
@@ -5,17 +5,17 @@ type User struct {
 	BaseModel
 
 	// 账号
-	UserId string `gorm:"column:user_id;not null;"`
+	UserId string `gorm:"type:varchar(255);not null;" json:"user_id"`
 
 	// 账号类型
-	UserType string `gorm:"column:user_type;not null;"`
+	UserType string `gorm:"type:varchar(255);not null;type:enum('oa','portal','virtual')" json:"user_type"`
 
 	// oa简要信息
-	Oa map[string]string `gorm:"column:oa;serializer:json"`
+	Oa string `json:"oa"`
 
 	// 被多少人关注
-	FollowerCount uint64 `gorm:"column:follower_count;default:0"`
+	FollowerCount uint64 `gorm:"type:int(10);unsigned;default:0" json:"follower_count"`
 
 	// 关注多少人
-	FollowingCount uint64 `gorm:"column:following_count;default:0"`
+	FollowingCount uint64 `gorm:"type:int(10);unsigned;default:0" json:"following_count"`
 }
