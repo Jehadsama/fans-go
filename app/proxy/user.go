a35fd6fb223b83b3a8ef8b0d56ff74538d5ee0d5
jehadsama <ayumijehad@qq.com>
2022-06-16 11:13:26 +0800

    feat(user): complete proxy user

--- app/proxy/user.go
+++ app/proxy/user.go
@@ -1,8 +1,31 @@
 package proxy
 
 import (
+	"fans-go/app/libs/utils"
 	"fans-go/app/models"
 )
+
+type Payload struct {
+	UserId   string            `json:"user_id"`
+	UserType string            `json:"user_type"`
+	Oa       map[string]string `json:"oa"`
+}
+
+func FindOneOrCreateDB(payload *Payload) *models.User {
+	baseQuery := map[string]interface{}{"user_id": payload.UserId, "user_type": payload.UserType}
+	var user *models.User
+	result := models.Model("user").Where(baseQuery).First(&user)
+	if result.RowsAffected == 1 {
+		return user
+	}
+	if payload.UserType == "oa" {
+		oaUsername := (&models.OaPGfUser{}).UsernameByOAID(payload.UserId)
+		payload.Oa = map[string]string{"username": oaUsername}
+	}
+
+	models.Model("user").Where(baseQuery).FirstOrCreate(&user)
+	return user
+}
 
 var followKeysMapping = map[string](map[string]string){
 	"source": {
@@ -15,53 +38,15 @@ var followKeysMapping = map[string](map[string]string){
 	},
 }
 
-type Payload struct {
-	UserId   string            `json:"user_id"`
-	UserType string            `json:"user_type"`
-	Oa       map[string]string `json:"oa"`
-}
-
-func FindOneOrCreateDB(payload *Payload) *models.User {
+func UpdateFollowCountByID(userObjectID, followType string) *models.User {
+	temp := followKeysMapping[followType]
+	var followCount int64
+	result := models.Model("relationship").Where(map[string]interface{}{"is_follow": true, temp["relationship"]: userObjectID}).Count(&followCount)
+	if result.Error != nil {
+		utils.CheckError("proxy,user,UpdateFollowCountByID", result.Error)
+	}
+	models.Model("user").Where("_id = ?", userObjectID).Update(temp["user"], followCount)
 	var user *models.User
-	result := models.Model(&user).Where("user_id = ? AND user_type = ?", payload.UserId, payload.UserType).First(&user)
-	if result.RowsAffected == 1 {
-		return user
-	}
-	if payload.UserType == "oa" {
-		oaUsername := (&models.OaPGfUser{}).UsernameByOAID(payload.UserId)
-		payload.Oa = map[string]string{"username": oaUsername}
-	}
-
-	models.Model(&user).Where(models.User{UserId: payload.UserId, UserType: payload.UserType}).FirstOrCreate(&user)
+	models.Model("user").Where("_id = ?", userObjectID).First(&user)
 	return user
-}
-
-
-const updateFollowCountByID = async ({
-	userObjectID,
-	followType = 'source',
-  }) => {
-	const followCount = await models.td_relationship.count({
-	  where: {
-		[followKeysMapping[followType].relationship]: userObjectID,
-		is_follow: true,
-	  },
-	});
-
-	await models.td_user.update(
-	  {
-		[followKeysMapping[followType].user]: followCount,
-	  },
-	  { where: { _id: userObjectID } }
-	);
-
-	return models.td_user.findOne({ where: { _id: userObjectID } });
-  };
-
-func UpdateFollowCountByID (userObjectID, followType string) *models.User{
-	if followType == "" {
-		followType= "source"
-	}
-
-
 }
