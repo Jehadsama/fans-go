6726ab1db59f0491207ad857841beb9a845bacc3
jehadsama <ayumijehad@qq.com>
2022-06-16 13:14:49 +0800

    feat(user): remove unused code

--- app/models/user.go
+++ app/models/user.go
@@ -42,24 +42,13 @@ type User struct {
 	UserType string `gorm:"type:varchar(255);not null;type:enum('oa','portal','virtual')" json:"user_type"`
 
 	// oa简要信息
-	Oa JSON `json:"oa"`
+	Oa map[string]string `gorm:"serialize:json" json:"oa"`
 
 	// 被多少人关注
 	FollowerCount uint64 `gorm:"type:int(10);unsigned;default:0" json:"follower_count"`
 
 	// 关注多少人
 	FollowingCount uint64 `gorm:"type:int(10);unsigned;default:0" json:"following_count"`
-}
-
-type SimpleUser struct {
-	// 账号
-	UserId string `gorm:"type:varchar(255);not null;" json:"user_id"`
-
-	// 账号类型
-	UserType string `gorm:"type:varchar(255);not null;type:enum('oa','portal','virtual')" json:"user_type"`
-
-	// 被多少人关注
-	FollowerCount uint64 `gorm:"type:int(10);unsigned;default:0" json:"follower_count"`
 }
 
 func (user *User) GetUsersByIds(ids []string) *gorm.DB {
